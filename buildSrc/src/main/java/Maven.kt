a23f699b358a97e28f763656ca04c031b717c572
guoyixiong <mostwantx@163.com>
2022-06-10 12:52:15 +0800

    新增配置： 弹幕库build.gradle改为使用groovy

--- buildSrc/src/main/java/Maven.kt
+++ buildSrc/src/main/java/Maven.kt
@@ -1,6 +1,6 @@
 object Maven {
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
-    const val GROUP = "com.github.4kp3"
+    const val GROUP = "com.github.fkp3"
     const val ARTIFACT_ID = "danmu"
 }
